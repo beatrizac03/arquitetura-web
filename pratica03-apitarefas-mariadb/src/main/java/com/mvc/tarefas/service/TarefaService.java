cab8722f12a00bbd7ca5236a9e2f5ffd2b7afaa1
beatrizac03 <beatrizaguiarc14@gmail.com>
2025-04-23 23:00:49 -0300

    pratica03-mariadb: setting .yml

--- pratica03-apitarefas-mariadb/src/main/java/com/mvc/tarefas/service/TarefaService.java
+++ pratica03-apitarefas-mariadb/src/main/java/com/mvc/tarefas/service/TarefaService.java
@@ -39,4 +39,4 @@ public class TarefaService {
         tarefaRepository.deleteById(id);
     }
 }
-}
+
--- pratica03-gerenciamento_de_tarefas/src/main/java/com/mvc/tarefas/repository/TarefaMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mvc.tarefas.repository;
-
-import com.mvc.tarefas.model.Tarefa;
-import org.springframework.data.jpa.repository.MongoRepository;
-
-public interface TarefaMongoRepository extends MongoRepository<Tarefa, String> {
-}
